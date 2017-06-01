4eb21bf820183e42c490c631274c0263a33eda30
devDB2017 <developerdombosco@gmail.com>
2017-06-01 18:19:09 -0300

    add

---
+++ js/myplaylist.js
@@ -0,0 +1,64 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'mix/Calvin Harris - My Way (Official Video).mp3',
+        title:'My Way',
+        artist:'Calvin Harris',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'4:05',
+        cover:'mix/Calvin Haris2.jpg'
+    },
+
+    {
+        mp3:'mix/edsherran.mp3',
+        title:'Shape of you',
+        artist:'Ed Sherran',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'4:23',
+        cover:'mix/ed sherran.jpg'
+    },
+    {
+        mp3:'mix/Martin Garrix & Bebe Rexha - In The Name Of Love (Official Video).mp3',
+        title:'In the name of love',
+        artist:'Martin Garrix',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'3:25',
+        cover:'mix/martingarrix.jpg'
+    },
+     {
+        mp3:'mix/Skrillex & Diplo - Mind feat. Kai (Official Video).mp3',
+        title:'Mind',
+        artist:'Skrillex & Diplo',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'3:25',
+        cover:'mix/skrillex.jpg'
+    },
+    {
+        mp3:'mix/twenty one pilots_ Lane Boy [OFFICIAL VIDEO] (2).mp3',
+        title:'Lane Boy',
+        artist:'twenty one pilots',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'3:25',
+        cover:'mix/twentyonepilonts.jpg'
+    }
+
+
+];
